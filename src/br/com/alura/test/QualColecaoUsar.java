be520ec5f5fba81eb5edb7fe65b54a30dac6f65e
Luiz Felipe <lffm1994@gmail.com>
2023-01-29 00:40:34 -0300

    Qual Collection usar

---
+++ src/br/com/alura/test/QualColecaoUsar.java
@@ -0,0 +1,15 @@
+package br.com.alura.test;
+
+import br.com.alura.model.Aluno;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class QualColecaoUsar {
+
+    public static void main(String[] args) {
+
+        Collection<Aluno> alunos = new ArrayList<>();
+        alunos.size();
+    }
+}
